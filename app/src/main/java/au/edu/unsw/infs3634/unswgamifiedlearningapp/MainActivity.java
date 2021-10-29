8555a51a1f38840b2ae06b564dd0a4012dd33ba5
joyaj <71417329+joyajeong@users.noreply.github.com>
2021-10-30 04:35:35 +1100

    recipes and recipe info api

--- app/src/main/java/au/edu/unsw/infs3634/unswgamifiedlearningapp/MainActivity.java
+++ app/src/main/java/au/edu/unsw/infs3634/unswgamifiedlearningapp/MainActivity.java
@@ -6,7 +6,9 @@ import androidx.room.Room;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
@@ -20,6 +22,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class MainActivity extends AppCompatActivity {
 
     //test push
+    private int recipeId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,20 +42,44 @@ public class MainActivity extends AppCompatActivity {
         });
 
         getRecipes();
+
     }
 
     //Gets recipes from API - just a test
     private void getRecipes() {
-        Call<RecipeSearchResult> call = RetrofitClient.getInstance().getMyApi().recipeResults();
+        Call<RecipeSearchResult> call = RetrofitClient.getInstance().getMyApi()
+                .recipeResults("pasta", "italian", null, true, 10, SpoonacularClient.apiKey);
+
         call.enqueue(new Callback<RecipeSearchResult>() {
             @Override
             public void onResponse(Call<RecipeSearchResult> call, Response<RecipeSearchResult> response) {
                 RecipeSearchResult recipes = response.body();
+                recipeId = recipes.getResults().get(0).getId();
+                getRecipeInfo(recipeId);
                 Log.i("Main Activity API Test", recipes.getResults().get(0).getTitle());
             }
 
             @Override
             public void onFailure(Call<RecipeSearchResult> call, Throwable t) {
+                Log.i("Main Activity API Test", "Failed" + t.getCause());
+
+            }
+        });
+    }
+
+    private void getRecipeInfo(int id) {
+        Call<RecipeInformationResult> call = RetrofitClient.getInstance().getMyApi()
+                .recipeInformationResults(id, SpoonacularClient.apiKey);
+
+        call.enqueue(new Callback<RecipeInformationResult>() {
+            @Override
+            public void onResponse(Call<RecipeInformationResult> call, Response<RecipeInformationResult> response) {
+                RecipeInformationResult recipeInfo = response.body();
+                Log.i("Main Activity API Test", recipeInfo.getSourceUrl());
+            }
+
+            @Override
+            public void onFailure(Call<RecipeInformationResult> call, Throwable t) {
                 Log.i("Main Activity API Test", "Failed" + t.getCause());
 
             }
--- app/src/main/java/au/edu/unsw/infs3634/unswgamifiedlearningapp/SpoonacularClient.java
+++ app/src/main/java/au/edu/unsw/infs3634/unswgamifiedlearningapp/SpoonacularClient.java
@@ -1,16 +1,38 @@
 package au.edu.unsw.infs3634.unswgamifiedlearningapp;
 
-import java.util.List;
+
+import java.util.Map;
 
 import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.http.FieldMap;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+import retrofit2.http.QueryMap;
 
 public interface SpoonacularClient {
 
     String API_BASE_URL = "https://api.spoonacular.com";
-    @GET("/recipes/complexSearch?query=pasta&maxFat=25&number=2&apiKey=43d6739df5224359ac578587bf27a807")
-    //Was Call<List<...>> but make sure to check if result is a list or just an object!!!
-    Call<RecipeSearchResult> recipeResults();
+    String apiKey = "43d6739df5224359ac578587bf27a807";
+
+    @GET("/recipes/complexSearch")
+    Call<RecipeSearchResult> recipeResults(
+            @Query("query") String query,
+            @Query("cuisine") String cuisine,
+            @Query("diet") String diet,
+            @Query("instructionsRequired") boolean instructionsRequired,
+            @Query("maxReadyTime") int maxReadyTime,
+            @Query("apiKey") String apiKey
+            );
+
+
+    @GET("/recipes/{id}/information")
+    Call<RecipeInformationResult> recipeInformationResults(
+            @Path("id") int recipeId,
+            @Query("apiKey") String apiKey
+
+    );
 
 }
