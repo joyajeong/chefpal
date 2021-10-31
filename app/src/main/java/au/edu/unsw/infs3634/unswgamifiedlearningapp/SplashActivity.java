f4d27f51475f5540bfc68d6ad58879ec3842b814
xulinyu2000 <xulinyu2000@gmail.com>
2021-10-31 20:33:15 +1100

    Splash

--- app/src/main/java/au/edu/unsw/infs3634/unswgamifiedlearningapp/SplashActivity.java
+++ app/src/main/java/au/edu/unsw/infs3634/unswgamifiedlearningapp/SplashActivity.java
@@ -1,14 +1,26 @@
 package au.edu.unsw.infs3634.unswgamifiedlearningapp;
+// opening
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.gson.TypeAdapterFactory;
 
 public class SplashActivity extends AppCompatActivity {
+    private TextView appName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        appName = findViewById(R.id.app_name);
+
+        //change font
+        Typeface typeface = ResourcesCompat.getFont(this,)
+
     }
 }
