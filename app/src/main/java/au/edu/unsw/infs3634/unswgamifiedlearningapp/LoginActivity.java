3f7a5049ddc454e48593509df18b3635e69e7f11
xulinyu2000 <xulinyu2000@gmail.com>
2021-11-02 00:58:08 +1100

    Login

--- app/src/main/java/au/edu/unsw/infs3634/unswgamifiedlearningapp/LoginActivity.java
+++ app/src/main/java/au/edu/unsw/infs3634/unswgamifiedlearningapp/LoginActivity.java
@@ -3,12 +3,60 @@ package au.edu.unsw.infs3634.unswgamifiedlearningapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText email, password;
+    private Button loginB;
+    private TextView forgotPassB, singupB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        loginB = findViewById(R.id.loginB);
+        singupB = findViewById(R.id.signupB);
+        forgotPassB = findViewById(R.id.forgot_pass);
+
+        //loginButton
+        loginB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //connect to firebase
+                //check validation
+                if(validateData()){
+                    login();
+
+                }
+
+            }
+        });
+
     }
+
+    private boolean validateData(){
+        //boolean status = false;
+        if(email.getText().toString().isEmpty()){
+            email.setError("Enter E-Mail ID");
+            return false;
+        }
+        if(password.getText().toString().isEmpty()){
+            password.setError("Enter Password");
+            return false;
+        }
+        return true;
+    }
+
+    private void login(){
+
+    }
+
+
 }
