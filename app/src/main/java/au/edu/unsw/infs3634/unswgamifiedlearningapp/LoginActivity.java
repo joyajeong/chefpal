99b86a636b577c2e9b1dbb581f05021ad991d9f2
xulinyu2000 <xulinyu2000@gmail.com>
2021-11-02 15:26:45 +1100

    SignUp

--- app/src/main/java/au/edu/unsw/infs3634/unswgamifiedlearningapp/LoginActivity.java
+++ app/src/main/java/au/edu/unsw/infs3634/unswgamifiedlearningapp/LoginActivity.java
@@ -2,6 +2,7 @@ package au.edu.unsw.infs3634.unswgamifiedlearningapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -35,7 +36,14 @@ public class LoginActivity extends AppCompatActivity {
                     login();
 
                 }
+            }
+        });
 
+        singupB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(intent);
             }
         });
 
@@ -53,9 +61,7 @@ public class LoginActivity extends AppCompatActivity {
         }
         return true;
     }
-
     private void login(){
-
     }
 
 
--- app/src/main/java/au/edu/unsw/infs3634/unswgamifiedlearningapp/SignUpActivity.java
+++ app/src/main/java/au/edu/unsw/infs3634/unswgamifiedlearningapp/SignUpActivity.java
@@ -2,13 +2,43 @@ package au.edu.unsw.infs3634.unswgamifiedlearningapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class SignUpActivity extends AppCompatActivity {
+    private EditText name,email,password, confirmPass;
+    private Button signUpB;
+    private ImageView backB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        name = findViewById(R.id.userName);
+        email = findViewById(R.id.emailID);
+        password = findViewById(R.id.password);
+        confirmPass = findViewById(R.id.confirm_pass);
+        backB = findViewById(R.id.backB);
+        signUpB = findViewById(R.id.signupB);
+
+        backB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        signUpB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               //connect to firebase
+            }
+        });
+
     }
 }
