a01ac47c5fee135502bfccd1d63af3bd2c605c4a
xulinyu2000 <xulinyu2000@gmail.com>
2021-11-10 14:36:25 +1100

    account

--- app/src/main/java/au/edu/unsw/infs3634/unswgamifiedlearningapp/FavoriteActivity.java
+++ app/src/main/java/au/edu/unsw/infs3634/unswgamifiedlearningapp/FavoriteActivity.java
@@ -67,6 +67,7 @@ public class FavoriteActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorite);
+        //
 
 //        if (getArguments() != null) {
 //            mParam1 = getArguments().getString(ARG_PARAM1);
