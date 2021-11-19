cca21b4772ecd4e84269cd35c3363a53f88d5ee0
xulinyu2000 <xulinyu2000@gmail.com>
2021-11-20 09:57:08 +1100

    score layout

--- app/src/main/java/au/edu/unsw/infs3634/unswgamifiedlearningapp/ScoreActivity.java
+++ app/src/main/java/au/edu/unsw/infs3634/unswgamifiedlearningapp/ScoreActivity.java
@@ -3,12 +3,37 @@ package au.edu.unsw.infs3634.unswgamifiedlearningapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ScoreActivity extends AppCompatActivity {
+    private TextView score, totalTime, totalQ, correctQ, wrongQ,unattempted;
+    private Button leaderB, reattemptedB, viewAnsB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score);
+        init();
+        loadData();
     }
+
+    private void init(){
+        score = findViewById(R.id.score);
+        totalQ = findViewById(R.id.totalQ);
+        totalTime = findViewById(R.id.totalTime);
+        correctQ = findViewById(R.id.correctQ);
+        wrongQ = findViewById(R.id.wrongQ);
+        unattempted = findViewById(R.id.unattempted);
+        leaderB = findViewById(R.id.checkRankB);
+        reattemptedB = findViewById(R.id.reattemptB);
+        viewAnsB = findViewById(R.id.viewAnsB);
+    }
+
+    private void loadData(){
+        int correctQ = 0, wrongQ = 0, unattempted = 0;
+
+    }
+
+
 }
